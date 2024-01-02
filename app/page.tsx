dc8df7ba26b1fadbcc541106c08a1c2244063deb
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-02 14:18:10 +0545

    homepage fetch and design

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,40 @@
  @returns {JSX.Element} The rendered page component.
  */
 
-import { Container } from '@/components'
-import Image from 'next/image'
-import Link from 'next/link'
-import { endpoint } from '@/utils/endpoint'
+import { Container } from "@/components";
+import Image from "next/image";
+import Link from "next/link";
+import { endpoint } from "@/utils/endpoint";
+
+async function getAllCharacters() {
+  const data = await fetch(`${endpoint}/characters`);
+  if (!data.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return data.json();
+}
 
 export default async function Page() {
+  const data = await getAllCharacters();
   return (
-      <main className="flex items-center justify-center h-screen">
-        <h1 className="text-3xl">Bird is the word</h1>
-      </main>
-  )
+    <main>
+      <Container className="grid grid-cols-2 gap-1 py-5 md:grid-cols-3 lg:grid-cols-4">
+        {data?.characters?.map((item: any) => (
+          <Link
+            key={item.name}
+            href={`/characters/${item.slug}`}
+            className="overflow-hidden rounded-md"
+          >
+            <Image
+              src={item.avatar}
+              alt=""
+              width={500}
+              height={500}
+              className="transition-all duration-500 hover:scale-110 hover:-rotate-2"
+            />
+          </Link>
+        ))}
+      </Container>
+    </main>
+  );
 }
